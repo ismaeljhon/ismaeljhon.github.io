3408e4dfce4c89c6b2baf6f9e1373fa06b70a44d
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-11-09 21:54:04 +0800

    add asests and homepage

---
+++ components/sections/ContactMe.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-sheet
+        id="contact"
+        color="#fff"
+        tag="section"
+        tile
+    >
+        <div class="py-12"></div>
+
+        <v-container>
+            <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">Contact Me</h2>
+
+            <v-responsive
+                class="mx-auto mb-5"
+                width="56"
+            >
+                <v-divider class="mb-1"></v-divider>
+                <v-divider></v-divider>
+            </v-responsive>
+
+            <v-theme-provider light>
+                <v-row>
+                    <v-col cols="12">
+                        <h3 class="text-center mb-5">via</h3>
+                        <p class="text-center">
+                            <a
+                                v-for="(item, index) in contactLinks"
+                                :href="item.link"
+                                :key="index"
+                                class="mr-3"
+                                target="blank"
+                            >
+                                <img :src="item.icon" width="32" />
+                            </a>
+                        </p>
+                    </v-col>
+                </v-row>
+            </v-theme-provider>
+        </v-container>
+
+        <div class="py-12"></div>
+    </v-sheet>
+</template>
+<script>
+export default {
+    name: 'contact-me',
+    data: () => ({
+        contactLinks: [
+            { name: 'email', link: 'mailto:jalbuna.ismael@gmail.com', icon: require('../../assets/email.png') },
+            { name: 'facebook', link: 'https://www.facebook.com/jhonismjr', icon: require('../../assets/facebook.png') },
+            { name: 'linkedin', link: 'https://www.linkedin.com/in/ismael-jhon-jr-jalbuna-57b62771/', icon: require('../../assets/linkedin.png') },
+        ]
+    })
+}
+</script>
---
+++ components/sections/Hero.vue
@@ -0,0 +1,58 @@
+<template>
+    <section id="hero">
+        <v-row no-gutters>
+            <v-img
+                :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+                gradient="to top right, rgba(0,0, 0,.33), rgba(0, 0, 0,.7)"
+                src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80"
+            >
+                <v-theme-provider dark>
+                    <v-container fill-height>
+                        <v-row
+                            align="center"
+                            class="white--text mx-auto"
+                            justify="center"
+                        >
+                            <v-col
+                                class="white--text text-center"
+                                cols="12"
+                                tag="h1"
+                            >
+                                <span
+                                    :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+                                    class="font-weight-light"
+                                >
+                                    Ismael Jhon Jalbuna Jr.
+                                </span>
+
+                                <br>
+
+                                <span
+                                    :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
+                                    class="font-weight-black"
+                                >
+                                    Full Stack Developer
+                                </span>
+
+                            </v-col>
+
+                            <v-btn
+                                class="align-self-end"
+                                fab
+                                outlined
+                                @click="$vuetify.goTo('#about-me')"
+                            >
+                                <v-icon>mdi-arrow-down</v-icon>
+                            </v-btn>
+                        </v-row>
+                    </v-container>
+                </v-theme-provider>
+            </v-img>
+        </v-row>
+    </section>
+</template>
+<script>
+export default {
+    name: 'hero'
+}
+</script>
