d5a1403ce431734285094081c6528cdbd1a767c9
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-11-18 13:29:06 +0800

    3e8565d

---
+++ _nuxt/5f090c3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{334:function(e,t,n){"use strict";n.r(t);var l={head:{title:"More About Me"},name:"About"},o=n(45),component=Object(o.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
